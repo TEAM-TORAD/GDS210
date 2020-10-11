340b7ffa3948e2be4d83cc1375a6c42c8ab688ef
Tom <tomtomcoromandel@gmail.com>
2020-10-12 09:26:29 +1100

    Script Fix

--- GAD210/Assets/OLOF/Scripts/Health.cs
+++ GAD210/Assets/OLOF/Scripts/Health.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+//Health fix
 public class Health : MonoBehaviour
 {
     public int health = 100;
--- GAD210/Assets/TOM/Scripts/PlayerAttacks.cs
+++ GAD210/Assets/TOM/Scripts/PlayerAttacks.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-
+//attack fix
 public class PlayerAttacks : MonoBehaviour
 {
     public Animator anim;
